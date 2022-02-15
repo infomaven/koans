e22686ddf72dee40b24ad8e66f05b9fb2d39e47c
Nadine Whitfield <nadine.whitfield@thoughtworks.com>
2022-02-15 13:02:16 -0800

    Tue: wip

--- triangle.rb
+++ triangle.rb
@@ -1,25 +1,23 @@
-# Triangle Project Code.
+#Triangle Project Code.
 
-# Triangle analyzes the lengths of the sides of a triangle
-# (represented by a, b and c) and returns the type of triangle.
-#
-# It returns:
-#   :equilateral  if all sides are equal
-#   :isosceles    if exactly 2 sides are equal
-#   :scalene      if no sides are equal
-#
-# The tests for this method can be found in
-#   about_triangle_project.rb
-# and
-#   about_triangle_project_2.rb
-#
-#
-#def triangle(a, b, c)
-#  if a == b and a == c and b == c 
-#    return true
-#  else
-#    return false
-#end
+
+#Triangle analyzes the lengths of the sides of a triangle
+(represented by a, b and c) and returns the type of triangle.
+
+#It returns:
+#  :equilateral  if all sides are equal
+#  :isosceles    if exactly 2 sides are equal
+#  :scalene      if no sides are equal
+
+
+
+
+def triangle(a, b, c)
+  if a == b and a == c and b == c 
+    return true
+  else
+    return false
+end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
