ca76aa7d09392c5953f9f4076c110303e858013e
Nadine Whitfield <nadine.whitfield@thoughtworks.com>
2022-02-15 19:40:51 -0800

    Tue: completed 154

--- triangle.rb
+++ triangle.rb
@@ -1,22 +1,21 @@
 #Triangle Project Code.
 
-
 #Triangle analyzes the lengths of the sides of a triangle
-(represented by a, b and c) and returns the type of triangle.
+# (represented by a, b and c) and returns the type of triangle.
 
 #It returns:
 #  :equilateral  if all sides are equal
 #  :isosceles    if exactly 2 sides are equal
 #  :scalene      if no sides are equal
 
-
-
-
 def triangle(a, b, c)
-  if a == b and a == c and b == c 
-    return true
+  if a == b and a == c and b == c
+    return :equilateral
+  elsif a != b and a != c and b != c
+    return :scalene
   else
-    return false
+    return :isosceles
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
